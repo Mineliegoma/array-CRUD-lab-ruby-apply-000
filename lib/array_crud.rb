ba8efe092dbebffaf40b458205ecbc38d83b44b9
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-23 23:52:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,8 +6,8 @@ def create_an_array
   nala=[1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  nala.push(element)
+def add_element_to_end_of_array(array, "element")
+  nala.push("element")
 end
 
 def add_element_to_start_of_array(array, element)
